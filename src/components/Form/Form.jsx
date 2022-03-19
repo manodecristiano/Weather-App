10738c4feebae1c43ea4a89f24703c0466a63cea
manodecristiano <cristian.carre.online@gmail.com>
2022-03-20 00:00:51 +0100

    Update Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import styles from './Form.module.css';
 
 const Form = () => {
+ 
+ const [location, setLocation] = useState('');
+
+ const onSubmit = event => {
+   event.preventDefault();
+   if(!location || location==='') return;
+
+ };
+
+
     return (
-        <form>
+        <form onSubmit={onSubmit}> 
             <input
                 aria-label="location"
                 type="text"
                 className={`${styles.input} form-control`}
                 placeholder="Search for location"
                 required
+                value={location}
+                onChange={event => setLocation(event.target.value)}
             />
 
-            <button type="submit" className={styles.button}>
+            <button type="submit" className={styles.button} onClick={onSubmit}>
                 SEARCH
             </button>
         </form>
--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,11 +1,26 @@
 import React, { Fragment } from 'react';
 
 import Header from '../Header';
+import styles from './Page.module.css'
+import Form from '../Form';
+import Error from '../Error';
+import Loader from '../Loader';
+import Forecast from '../Forecast';
 
 const Page = () => {
     return (
         <Fragment>
             <Header />
+            <div className={styles.box}>
+            {/* Form */}
+             <Form />
+              {/* Error */}
+              {/* <Error /> */}
+              {/* Loader */}
+              {/* <Loader /> */}
+              {/* Forecast */}
+              {/* <Forecast /> */}
+              </div>
         </Fragment>
     );
 };
